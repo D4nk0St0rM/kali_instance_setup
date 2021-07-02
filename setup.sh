904a3ac07a3bd8568977cb7c73551bd78097b51e
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-07-02 12:09:05 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -240,7 +240,7 @@ sudo apt-get install -y -qq apt-transport-https
 echo -e "\n ${GREEN}[+]${RESET} Installation of applications ${GREEN}MS visual code studio ${RESET}"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get install -y -qq code
 sudo rm microsoft.gpg
 
