b583b569a3bbab2dd93df2ff2b6e9be0f63fec25
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2021-07-02 12:18:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ sudo mv sources.list $file
 #### Add repo keys
 wget -q -O - https://repo.protonvpn.com/debian/public_key.asc | sudo tee -a /usr/share/keyrings/protonvpn.asc
 sudo apt-get -qq update
-sudo apt-get -y -qq full-upgrade 
+sudo apt-get -y -qq dist-upgrade 
 sudo apt-get install -y -qq kali-linux-large
 sudo apt-get install -y -qq software-properties-common
 sudo apt-get install -y -qq gnupg-agent
