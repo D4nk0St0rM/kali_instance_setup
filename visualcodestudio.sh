89273865f70a04890d255d37b69fdcc2863faf3b
D4nk0St0rM <76242256+D4nk0St0rM@users.noreply.github.com>
2022-05-16 22:21:44 +0100

    visualcodestudio.sh

---
+++ visualcodestudio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install curl gpg gnupg2 software-properties-common apt-transport-https
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update
+sudo apt install code
